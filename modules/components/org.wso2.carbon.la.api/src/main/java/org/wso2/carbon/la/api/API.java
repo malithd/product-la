71ffead9ae195b86ad5da96a63711ae25d13abba
apremalal <anuruddhapremalal@gmail.com>
2015-11-21 16:20:32 +0530

    adding python and java agents

---
+++ modules/components/org.wso2.carbon.la.api/src/main/java/org/wso2/carbon/la/api/API.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.la.api;
+
+public class API {
+    public static void getSampleEvents(int eventCount){
+
+    }
+}
---
+++ modules/components/org.wso2.carbon.la.restapi/src/main/java/org/wso2/carbon/la/restapi/LASRestAPIV10.java
@@ -0,0 +1,24 @@
+package org.wso2.carbon.la.restapi;
+
+/**
+ * Created by anuruddha on 11/9/15.
+ */
+public class LASRestAPIV10 {
+
+    public void uploadFile(){
+
+    }
+
+    public void getSampleEvents(){
+
+    }
+
+    public void saveOrUpdateConfiguration(){
+
+    }
+
+    public void getConfiguration(){
+
+    }
+
+}
